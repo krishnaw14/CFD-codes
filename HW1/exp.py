26bcca720233d1aaf2ce80d24446be2fec4d1ce2
krishnaw14 <krishnaw14@gmail.com>
2019-01-23 18:16:09 +0530

    exp(x) usinng taylor series

--- HW1/exp.py
+++ HW1/exp.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import argparse
 
 def exp(x = 10):
 
@@ -6,10 +7,7 @@ def exp(x = 10):
 	sum_terms = 0
 	i = 1
 	term = 1
-	while (term != float("inf") and term != float("nan")):
-		# print(term)
-		# if term == 0:
-		# 	break
+	while (term != float("inf") and term != float("nan") and term != 0):
 		sum_terms += term
 		term *= x/i
 		num_terms += 1
@@ -17,6 +15,19 @@ def exp(x = 10):
 
 	return sum_terms, num_terms
 
-print(exp(-5)[0], np.exp(-5))
-print(exp(-10)[0], np.exp(-10))
-print(exp(-50)[0], np.exp(-50))
+
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(description='x for which exp(x) is to be computed')
+	parser.add_argument("-x", type = float, default = -5, help='x for which exp(x) is to be computed')
+	args = parser.parse_args()
+
+	answer, num_terms = exp(args.x)
+	exact_value = np.exp(args.x)
+	error = 100*abs(answer - exact_value)/exact_value
+
+	print("x = ", args.x)
+	print("Obtained Value: ", answer) 
+	print("Exact Value:", exact_value)
+	print("Error (%) : ", error )
+	print("Number of terms: ", num_terms, "\n")
