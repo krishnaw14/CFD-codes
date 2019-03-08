88044993e710ab6b60582e30f78ef1efefa771d7
krishnaw14 <krishnaw14@gmail.com>
2019-03-08 11:17:20 +0530

    Question 2 done

--- HW3/q2_part2.py
+++ HW3/q2_part2.py
@@ -4,12 +4,12 @@ import os
 
 # To initialize boundary conditions and grid points
 def initialize_grid(N):
-	grid_points = np.linspace(0,1,N)
+	grid_points = np.linspace(-1,1,N)
 	delta_x = 1/(N-1)
-	u_values = np.sin(grid_points*2*np.pi)
-	u_values[0] = 0
-	u_values[-1] = u_values[0] 
-
+	u_values = np.zeros(N)
+	for i in range(N):
+		if abs(grid_points[i]) < 0.5:
+			u_values[i] = np.cos(np.pi*grid_points[i])**2
 	return grid_points, u_values, delta_x
 
 # Defining various numerical schemes
@@ -21,19 +21,10 @@ def solve_FTBS(u_values, cfl, N):
 	new_values[-1] = new_values[1]
 	return new_values
 
-def solve_FTFS(u_values, cfl, N):
+def solve_FTCS2(u_values, cfl, N):
 	new_values = np.array(u_values)
 	for i in range(1,N-1):
-		new_values[i] = u_values[i] - cfl*(u_values[i+1] - u_values[i])
-	new_values[0] = new_values[-2]
-	new_values[-1] = new_values[1]
-	return new_values
-
-def solve_FTCS(u_values, cfl, N):
-	new_values = np.array(u_values)
-	for i in range(1,N-1):
-		new_values[i] = u_values[i] - 0.5*cfl*(u_values[i+1] - u_values[i-1])
-	# new_values[-1] = new_values[0]
+		new_values[i] = (1-cfl*cfl)*new_values[i] - 0.5*cfl*(1-cfl)*new_values[i+1] + 0.5*cfl*(1+cfl)*new_values[i-1]
 	new_values[0] = new_values[-2]
 	new_values[-1] = new_values[1]
 	return new_values
@@ -56,19 +47,19 @@ def solve_and_plot(scheme_name, solve_scheme, grid_points, u_values, delta_x, si
 	# plt.axis([0,1,-1,1])
 	# plt.show()
 	plt.savefig(saved_graphs_directory + "Scheme = {} CFL value = {} (t = {}s).png".format(scheme_name, cfl, simulated_time))
-	plt.pause(0.5)
+	plt.pause(0.1)
 	plt.clf()
 
 
-N = 101
-cfl = 0.5
-simulated_time_values = [0, 1, 1.5, 3, 4]
+N = 81
+cfl_values = [0.4, 0.7]
+simulated_time_values = [2, 4, 6]
 
 # Intialize domain and initial and boundary conditions
 grid_points, u_values, delta_x = initialize_grid(N)
 
 # Define directory to save all the graphs
-saved_graphs_directory = "saved_graphs_question2/"
+saved_graphs_directory = "saved_graphs_question2_part_2/"
 if not os.path.exists(saved_graphs_directory):
 	os.mkdir(saved_graphs_directory)
 
@@ -79,42 +70,33 @@ plt.xlabel("x")
 plt.ylabel("u(x,t)")
 # plt.axis([0,1,min(u_values)/1.2, max(u_values)*1.2])
 plt.savefig(saved_graphs_directory + "InitialBC.png")
-plt.pause(1)
+# plt.pause(1)
+plt.show()
 plt.clf()
 
-
-# Solve for FTCS
-scheme_name = "FTCS"
+# # Solving for FTCS2 scheme
+scheme_name = "FTCS2"
 print("##########################")
-print("Solving for FTCS scheme:")
+print("Solving for FTCS2 scheme:")
 print("##########################\n")
-print("CFL value = ", cfl, "\n")
-for simulated_time in simulated_time_values:
-	print("Simulating for t = ", simulated_time)
-	solve_and_plot(scheme_name, solve_FTCS, grid_points, u_values, delta_x, simulated_time, cfl, N)
-	print("\n")
-
-# # # Solve for FTFS
-scheme_name = "FTFS"
-print("##########################")
-print("Solving for FTFS scheme:")
-print("##########################\n")
-print("CFL value = ", cfl, "\n")
-for simulated_time in simulated_time_values:
-	print("Simulating for t = ", simulated_time)
-	solve_and_plot(scheme_name, solve_FTFS, grid_points, u_values, delta_x, simulated_time, cfl, N)
-	print("\n")
+for cfl in cfl_values:
+	print("CFL value = ", cfl, "\n")
+	for simulated_time in simulated_time_values:
+		print("Simulating for t = ", simulated_time)
+		solve_and_plot(scheme_name, solve_FTCS2, grid_points, u_values, delta_x, simulated_time, cfl, N)
+		print("\n")
 
 # # Solving for FTBS scheme
 scheme_name = "FTBS"
 print("##########################")
 print("Solving for FTBS scheme:")
 print("##########################\n")
-print("CFL value = ", cfl, "\n")
-for simulated_time in simulated_time_values:
-	print("Simulating for t = ", simulated_time)
-	solve_and_plot("FTBS", solve_FTBS, grid_points, u_values, delta_x, simulated_time, cfl, N)
-	print("\n")
+for cfl in cfl_values:
+	print("CFL value = ", cfl, "\n")
+	for simulated_time in simulated_time_values:
+		print("Simulating for t = ", simulated_time)
+		solve_and_plot(scheme_name, solve_FTBS, grid_points, u_values, delta_x, simulated_time, cfl, N)
+		print("\n")
 
 
 
