Corrected formula

HW4/advection_vectorized.py
@@ -65,11 +65,11 @@ def solve_iteration(u_values, N, dt, dx, dy, simulation_time, Lambda, scheme_nam
 
 	for iteration in range(num_iterations):
 
-		f_r = np.array(0.5*(u_values[2:,1:N+1] + u_values[1:N+1,1:N+1]) - 0.5*Lambda*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1]))
-		f_l = np.array(0.5*(u_values[1:N+1,1:N+1] + u_values[0:N,1:N+1]) - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1]))
+		f_r = 0.5*(u_values[2:,1:N+1] + u_values[1:N+1,1:N+1]) - 0.5*Lambda*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])
+		f_l = 0.5*(u_values[1:N+1,1:N+1] + u_values[0:N,1:N+1]) - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])
 
-		g_r = np.array(0.5*(u_values[1:N+1,2:] + u_values[1:N+1,1:N+1]) - 0.5*Lambda*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1]))
-		g_l = np.array(0.5*(u_values[1:N+1,1:N+1] + u_values[1:N+1,0:N]) - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N]))
+		g_r = 0.5*(u_values[1:N+1,2:] + u_values[1:N+1,1:N+1]) - 0.5*Lambda*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])
+		g_l = 0.5*(u_values[1:N+1,1:N+1] + u_values[1:N+1,0:N]) - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])
 
 		new_u_values[1:N+1, 1:N+1] = np.array(u_values[1:N+1, 1:N+1] - (dt/dx)*(f_r-f_l) - (dt/dy)*(g_r-g_l))
 

HW4/burger.py
@@ -11,7 +11,7 @@ def initialize_grid(N, A):
 	u_values = np.zeros((N,N))
 
 	x_values = np.linspace(0,15,N)
-	y_values = np.linspace(0,15, N)
+	y_values = np.linspace(0,15,N)
 
 
 	for i in range(N):
@@ -19,7 +19,7 @@ def initialize_grid(N, A):
 			x = x_values[i]
 			y = y_values[j]
 
-			u_values[i][j] = A*np.exp(-0.5*(x-10)**2 - 0.5*(y-10)**2)
+			u_values[i][j] = A*np.exp(-0.5*(x-5)**2 - 0.5*(y-5)**2)
 
 	return u_values
 
@@ -39,10 +39,10 @@ def add_ghost_cells(u_values, N):
 	return u_values
 
 
-def solve_lax_friedrichs(u_values, N, dt, dx, dy, simulation_time):
+def solve_iteration(u_values, N, dt, dx, dy, nu, Lambda, simulation_time, scheme_name):
 
 	num_iterations = int(simulation_time/dt)
-	print("Simulation Time = {}. Number of iterations = {}".format(simulation_time, num_iterations))
+	print("Simulating for t = {}s. Number of iterations = {}".format(simulation_time, num_iterations))
 
 	new_u_values = np.array(u_values)
 
@@ -56,21 +56,27 @@ def solve_lax_friedrichs(u_values, N, dt, dx, dy, simulation_time):
 	Y = np.linspace(0,15,N)
 	X, Y = np.meshgrid(X, Y)
 
+	plt.tight_layout() 
 	surf = ax.plot_surface(X, Y, new_u_values[1:N+1, 1:N+1], cmap=cm.coolwarm,
                         linewidth=0, antialiased=False)
+	ax.set_xlabel('$X$', fontsize=20)
+	ax.set_ylabel('$Y$', fontsize=20)
+	ax.set_zlabel('$U$', fontsize=20)
 	plt.draw()
 	plt.pause(0.5)
-	Lambda = dt/dx
 
 	for iteration in range(num_iterations):
 
-		f_r = 0.25*(u_values[2:,1:N+1]**2 + u_values[1:N+1,1:N+1]**2) -0.02*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx - 0.02*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx - 0.5*Lambda*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])
-		f_l = 0.25*(u_values[1:N+1,1:N+1]**2 + u_values[0:N,1:N+1]**2) -0.02*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx - 0.02*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])
+		if scheme_name == "Lax-Friedrich":
+			Lambda = u_values[1:N+1,1:N+1]
 
-		g_r = 0.25*(u_values[1:N+1,2:]**2 + u_values[1:N+1,1:N+1]**2) -0.02*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy - 0.02*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy - 0.5*Lambda*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])
-		g_l = 0.25*(u_values[1:N+1,1:N+1]**2 + u_values[1:N+1,0:N]**2) -0.02*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy - 0.02*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])
+		f_r = 0.25*(u_values[2:,1:N+1]**2 + u_values[1:N+1,1:N+1]**2) -nu*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx  - 0.5*Lambda*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])
+		f_l = 0.25*(u_values[1:N+1,1:N+1]**2 + u_values[0:N,1:N+1]**2) -nu*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx  - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])
 
-		new_u_values[1:N+1, 1:N+1] = u_values[1:N+1, 1:N+1] - (dt/dx)*(f_r-f_l) - (dt/dy)*(g_r-g_l)
+		g_r = 0.25*(u_values[1:N+1,2:]**2 + u_values[1:N+1,1:N+1]**2) -nu*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy - 0.5*Lambda*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])
+		g_l = 0.25*(u_values[1:N+1,1:N+1]**2 + u_values[1:N+1,0:N]**2) -nu*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy - 0.5*Lambda*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])
+
+		new_u_values[1:N+1, 1:N+1] = np.array(u_values[1:N+1, 1:N+1] - (dt/dx)*(f_r-f_l) - (dt/dy)*(g_r-g_l))
 
 		new_u_values[0] = new_u_values[1]
 		new_u_values[N+1] = new_u_values[N]
@@ -80,25 +86,93 @@ def solve_lax_friedrichs(u_values, N, dt, dx, dy, simulation_time):
 
 		u_values = np.array(new_u_values)
 
-	surf.remove()
-	surf = ax.plot_surface(X, Y, new_u_values[1:N+1, 1:N+1], cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+		if iteration%(num_iterations/10) == 0:
 
-	plt.draw()
-	plt.show()
+			surf.remove()
+			plt.tight_layout() 
+			surf = ax.plot_surface(X, Y, new_u_values[1:N+1, 1:N+1], cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+			ax.set_title("Simulating for t = {}s: Scheme: {}, ν: {}\n".format(simulation_time, scheme_name, nu), fontsize="15", y=1.08)
+			plt.draw()
+			plt.pause(0.1)
+
 	surf.remove()
+	plt.tight_layout() 
+	surf = ax.plot_surface(X, Y, u_values[1:N+1, 1:N+1], cmap = cm.coolwarm, 
+		linewidth = 0, antialiased=False)
+	ax.set_title("3D Plot after t = {}s: Scheme: {}, ν: {}\n".format(simulation_time, scheme_name, nu), fontsize="15", y=1.08)
+	plt.draw()
+	plt.savefig(saved_plots_dir+"burger_{}_t_{}.png".format(scheme_name, simulation_time))
+	surf.remove()
+	print("Plot saved!")
+	plt.close()
+
+	return u_values
 
 
 N = 101
 A = 2
-dt = 0.01
+dt = 0.001
 dx = 15/(N-1)
 dy = 15/(N-1)
+nu_values = [0,0.02,0.04]
 
-time_values = [10,20,30,40]
+
+time_values = [0,5,15,25]
 initial_u_values = initialize_grid(N,A)
 initial_u_values = add_ghost_cells(initial_u_values, N)
-# print(np.linspace(0,20,N)[25])
+saved_plots_dir = "burger_plots/"
+if not os.path.exists(saved_plots_dir):
+	os.mkdir(saved_plots_dir)
 
+print("Solving For FTBS... ")
+Lambda = 1
 for time in time_values:
-	solve_lax_friedrichs(initial_u_values, N, dt, dx, dy, time)
+	for nu in nu_values:
+		print("nu =", nu)
+		u_values = solve_iteration(initial_u_values, N, dt, dx, dy, nu, Lambda, time, "FTBS")
+		fig_2d = plt.figure()
+		plt.plot(np.linspace(0,15,N), u_values.diagonal()[1:-1])
+		plt.xlabel("x")
+		plt.ylabel("U")
+		plt.title("Diagonal Plot t = {}s".format(time))
+		plt.savefig(saved_plots_dir+"burger_diagonal_{}_t_{}.png".format("FTBS", time))
+		plt.close()
+
+print("Diagonal Plots have been generated!\n\n")
+
+print("Solving For Lax Friedrich... ")
+Lambda = 0 # Lambda is initialized in solve_iteration function for this scheme. Here 0 is just a garbage value.
+for time in time_values:
+	for nu in nu_values:
+		print("nu =", nu)
+		u_values = solve_iteration(initial_u_values, N, dt, dx, dy, nu, Lambda, time, "Lax-Friedrich")
+		fig_2d = plt.figure()
+		plt.plot(np.linspace(0,15,N), u_values.diagonal()[1:-1])
+		plt.xlabel("x")
+		plt.ylabel("U")
+		plt.title("Diagonal Plot t = {}s".format(time))
+		plt.savefig(saved_plots_dir+"burger_diagonal_{}_t_{}.png".format("Lax-Friedrich", time))
+		plt.close()
+
+print("Diagonal Plots have been generated!\n\n")
+
+print("Solving For FTCS2... ")
+Lambda = dt/dx
+for time in time_values:
+	for nu in nu_values:
+		print("nu =", nu)
+		u_values = solve_iteration(initial_u_values, N, dt, dx, dy, nu, Lambda, time, "FTCS2")
+		fig_2d = plt.figure()
+		plt.plot(np.linspace(0,15,N), u_values.diagonal()[1:-1])
+		plt.xlabel("x")
+		plt.ylabel("U")
+		plt.title("Diagonal Plot t = {}s".format(time))
+		plt.savefig(saved_plots_dir+"burger_diagonal_{}_t_{}.png".format("FTCS2", time))
+		plt.close()
+
+print("Diagonal Plots have been generated!")
+
+
+
+	

HW4/diffusion.py
@@ -67,43 +67,44 @@ def solve_iteration(u_values, N, dt, dx, dy, gamma, simulation_time):
 
 	for iteration in range(num_iterations):
 
-		f_r = -gamma*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx - gamma*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx
-		f_l = -gamma*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx - gamma*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx
+		f_r = - gamma*(u_values[2:,1:N+1] - u_values[1:N+1,1:N+1])/dx
+		f_l = - gamma*(u_values[1:N+1,1:N+1] - u_values[0:N,1:N+1])/dx
 
-		g_r = -gamma*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy - gamma*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy
-		g_l = -gamma*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy - gamma*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy
+		g_r = - gamma*(u_values[1:N+1,2:] - u_values[1:N+1,1:N+1])/dy
+		g_l = - gamma*(u_values[1:N+1,1:N+1] - u_values[1:N+1,0:N])/dy
 
-		new_u_values[1:N+1, 1:N+1] = u_values[1:N+1, 1:N+1] - (dt/dx)*(f_r-f_l) - (dt/dy)*(g_r-g_l)
+		new_u_values[1:N+1, 1:N+1] = np.array(u_values[1:N+1, 1:N+1] - (dt/dx)*(f_r-f_l) - (dt/dy)*(g_r-g_l))
 
-		new_u_values[0] = new_u_values[1]
-		new_u_values[N+1] = new_u_values[N]
+		new_u_values[0] = np.array(new_u_values[1])
+		new_u_values[N+1] = np.array(new_u_values[N])
 
-		new_u_values[:,0] = new_u_values[:, 1]
-		new_u_values[:,N+1] = new_u_values[:, N]
+		new_u_values[:,0] = np.array(new_u_values[:, 1])
+		new_u_values[:,N+1] = np.array(new_u_values[:, N])
 
 		u_values = np.array(new_u_values)
 
+		# if iteration%500 == 0:
 	surf.remove()
 	surf = ax.plot_surface(X, Y, new_u_values[1:N+1, 1:N+1], cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 	ax.set_title("3D Plot after t = {}s: ".format(simulation_time))
-	plt.savefig(saved_plots_dir + "diffusiont_{}.png".format(simulation_time))
+	# plt.savefig(saved_plots_dir + "diffusiont_{}.png".format(simulation_time))
 
 	plt.draw()
-	plt.pause(1)
-	surf.remove()
+	plt.pause(0.5)
+		# surf.remove()
 
 	return u_values
 
 
 N = 101
 A = 2
-dt = 0.001
+dt = 0.01
 dx = 20/(N-1)
 dy = 20/(N-1)
 gamma = 0.1
 
-time_values = [0,2.5,5,7.5,10,12.5,15,17.5,20,22.5,25]
+time_values = [10,20,30,40]
 initial_u_values = initialize_grid(N,A)
 initial_u_values = add_ghost_cells(initial_u_values, N)
 
@@ -119,7 +120,7 @@ for time in time_values:
 	plt.ylabel("U")
 	plt.title("Diagonal Plot t = {}s".format(time))
 	plt.axis([0,20,0,2])
-	plt.savefig(saved_plots_dir+"diagonal_{}_t_{}.png".format("FTBS", time))
+	plt.savefig(saved_plots_dir+"diagonal_t_{}.png".format(time))
 	plt.close()
 
 print("Diagonal Plots have been saved!")